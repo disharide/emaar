33a55424b017d002f85ac45bd36251db35a32fcf
Dishari <de.dishari@gmail.com>
2016-05-12 19:01:12 +0530

    Start

--- app/services/StockUpdateServiceImpl.java
+++ app/services/StockUpdateServiceImpl.java
@@ -1,41 +1,34 @@
 package services;
 
-import java.io.IOException;
-import java.util.Date;
-
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpGet;
-import org.apache.http.impl.client.DefaultHttpClient;
+import java.util.Random;
 
 import play.Logger;
-
-import com.fasterxml.jackson.databind.ObjectMapper;
-
 import entity.EPStockRecord;
 import entity.PriceChangeDirection;
 
-@SuppressWarnings("deprecation")
 public class StockUpdateServiceImpl {
 
 	public boolean stockFetchAndUpdate() {
-		HttpClient httpClient = new DefaultHttpClient();
+		// HttpClient httpClient = new DefaultHttpClient();
 
 		boolean isUpdated = false;
 
 		try {
 
-			HttpGet httpGet = new HttpGet(
-					"http://demo6504781.mockable.io/get/stock");
-
-			HttpResponse response = httpClient.execute(httpGet);
+			/*
+			 * HttpGet httpGet = new HttpGet(
+			 * "http://demo6504781.mockable.io/get/stock");
+			 * 
+			 * HttpResponse response = httpClient.execute(httpGet);
+			 */
 
 			EPStockRecord epStockRecord = EPStockRecord.find.order("id desc")
 					.setMaxRows(1).findUnique();
 
-			EPStockRecord incomingEpStockRecord = new ObjectMapper().readValue(
-					response.getEntity().getContent(), EPStockRecord.class);
+			EPStockRecord incomingEpStockRecord = new EPStockRecord();
+
+			incomingEpStockRecord.stockPrice = Double
+					.parseDouble(getRandomValue(5, 7, 2));
 
 			if (epStockRecord == null
 					|| epStockRecord.stockPrice != incomingEpStockRecord.stockPrice) {
@@ -65,14 +58,25 @@ public class StockUpdateServiceImpl {
 			Logger.info("stock price " + epStockRecord.stockPrice);
 			Logger.info("incoming stock price "
 					+ incomingEpStockRecord.stockPrice);
-		} catch (ClientProtocolException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
+		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 
 		return isUpdated;
+	}
+
+	private String getRandomValue(final int lowerBound, final int upperBound,
+			final int decimalPlaces) {
+
+		if (lowerBound < 0 || upperBound <= lowerBound || decimalPlaces < 0) {
+			throw new IllegalArgumentException("Put error message here");
+		}
+
+		final double dbl = (new Random().nextDouble() //
+				* (upperBound - lowerBound))
+				+ lowerBound;
+		return String.format("%." + decimalPlaces + "f", dbl);
+
 	}
 }
